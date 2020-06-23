c1114f3370a16a49d90c7353b1dde3ec91f04aea
Frontend <frontend@pinky.amoobae.net>
2020-06-23 20:18:17 +0530

    Created Project

---
+++ MovieBrowser/MovieBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieBrowser
+//
+//  Created by Frontend on 6/23/20.
+//  Copyright © 2020 Frontend. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
