c7a1de9d473b1f51f57173d89556b2f96adae6fa
Frontend <frontend@pinky.amoobae.net>
2020-06-24 19:17:55 +0530

    SearchResults model added

---
+++ MovieBrowser/MovieBrowser/Models/SearchResults.swift
@@ -0,0 +1,41 @@
+//
+//  SearchResults.swift
+//  MovieBrowser
+//
+//  Created by Frontend on 6/23/20.
+//  Copyright © 2020 Frontend. All rights reserved.
+//
+
+import UIKit
+
+struct SearchResults{
+    
+    var title: String?
+    var id: Int?
+    var posterUrl: String?
+    
+    /*
+     {
+         "popularity": 28.406,
+         "vote_count": 2467,
+         "video": false,
+         "poster_path": "/cAh2pCiNPftsY3aSqJuIOde7uWr.jpg",
+         "id": 1903,
+         "adult": false,
+         "backdrop_path": "/wwLufumafJojc59hgIamHyJSTO9.jpg",
+         "original_language": "en",
+         "original_title": "Vanilla Sky",
+         "genre_ids": [
+             18,
+             14,
+             878,
+             53,
+             10749
+         ],
+         "title": "Vanilla Sky",
+         "vote_average": 6.8,
+         "overview": "David Aames has it all: wealth, good looks and gorgeous women on his arm. But just as he begins falling for the warmhearted Sofia, his face is horribly disfigured in a car accident. That's just the beginning of his troubles as the lines between illusion and reality, between life and death, are blurred.",
+         "release_date": "2001-01-22"
+     }
+     */
+}
