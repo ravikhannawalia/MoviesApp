22242b2a5ec5431a70823c3f70f22de19716de11
ravikhannawalia <ravikhannawalia@D11-RaviKhannawalia.local>
2020-06-28 17:28:31 +0530

    new view contolled added

---
+++ MovieBrowser/MovieBrowser/Scenes/SimpleCell.swift
@@ -0,0 +1,14 @@
+//
+//  SimpleCell.swift
+//  MovieBrowser
+//
+//  Created by Ravi Khannawalia on 28/06/20.
+//  Copyright © 2020 Frontend. All rights reserved.
+//
+
+import UIKit
+
+class SimpleCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
--- MovieBrowser/MovieBrowser/ViewController.swift
+++ MovieBrowser/MovieBrowser/ViewController.swift
@@ -8,7 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDataSourcePrefetching {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SimpleCell", for: indexPath) as? SimpleCell  else {
+               fatalError("The dequeued cell is not an instance of SearchResultCell")
+           }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        print("Okay!")
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
